ce6cbe002cd281095a48940e61c6dd8ff6bf452a
ricardo <ricardo.ernesto.melara@clases.edu.sv>
2026-09-17 16:34:14 -0600

    Agregado index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+2
+ 
+3
+header('Content-Type: application/json');
+4
+ 
+5
+echo json_encode([
+6
+"success" => true,
+7
+"message" => "API Asistencia funcionando correctamente"
+8
+]);
+9
+ 
+10
+?>
